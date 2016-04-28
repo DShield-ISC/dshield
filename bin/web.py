2b94264600f3ad566d6322efdcfd4a89382134d8
Johannes Ullrich <jullrich@sans.edu>
2016-04-28 11:01:19 +0000

    quick commit before adding more sql to web.py

--- bin/web.py
+++ bin/web.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from datetime import datetime
+import MySQLdb
+
 
 PORT_NUMBER = 8080
 
 #This class will handles any incoming request from
 #the browser
 class myHandler(BaseHTTPRequestHandler):
-
-    #Handler for the GET requests
-    def do_GET(self):
-        print (self.path)
-        print (self.request_version)
-        print (self.headers)
+    # emulate webcam
+    def sendgoaheadwebsheaders(self):
+        self.protocol_version='HTTP/1.0'
+        self.server_version='GoAhead-Webs/2.5.0'
+        self.sys_version=''
+        self.send_response(200)
+        self.send_header('Pragma','no-cache')
+        self.send_header('Cache-Control','no-cache')
+        self.send_header('Content-type','text/html')
+        
+    def sendheaders(self):
         self.protocol_version='HTTP/1.1'
         self.server_version='mini_httpd/1.19'
         self.sys_version='19dec2003'
         self.send_response(200)
         self.send_header('Content-type','text/html; charset=%s')
-        self.send_header('Content-Length','440');
         self.send_header('Last-Modified',datetime.today().strftime('%a, %d %b %Y %H:%M:%S GMT'))
         self.send_header('Connection','close');
         self.end_headers()
-        # Send the html message
+    def log_message(self, format, *args):
+        return
+    #Handler for the GET requests
+    def do_GET(self):
+
+        self.sendgoaheadwebsheaders(self)
         self.wfile.write('''<HTML>
 <HEAD><TITLE>Index of ./</TITLE></HEAD>
 <BODY BGCOLOR="#99cc99" TEXT="#000000" LINK="#2020ff" VLINK="#4040cc">
