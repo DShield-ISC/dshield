f05c67eef2f33c806001ebffeb4a8ed4ad2c14dc
Johannes B. Ullrich <jullrich@sans.edu>
2020-11-09 11:31:27 -0500

    added systemctl files

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -7,16 +7,23 @@ if [ ! "$userid" = "0" ]; then
    echo ${LINE}
    exit 9
 fi
-
 systemctl stop cowrie
-
+systemctl stop webpy
 for b in `ps -ef | grep '^cowrie' | awk '{print $2}'`; do kill -9 $b; done
-
 rm -rf /srv/*
 rm -rf /etc/dshield.ini.*
 mv /etc/dshield.ini /etc/dshield.ini.backup
 rm -rf /etc/cron.d/dshield
 rm -rf /etc/rsyslog.d/dshield.conf
 rm -rf /var/run/dshield
+rm -rf /lib/systemd/system/cowrie.service
+rm -rf /lib/systemd/system/webpy.service
+rm -rf /etc/network/iptables
+systemctl enable ufw
 deluser cowrie
 echo "Done. Please reboot. The SSH daemon is still listening on port 12222"
+
+
+
+
+
