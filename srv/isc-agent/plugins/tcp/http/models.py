2ddceb50b1d443b1774e26016bcac1e590ab5364
jdrew-sans <jdrew@sans.org>
2022-01-26 09:01:27 -0600

    ISC-10 #WIP Created Sigs, HdrResponses, and paths models.

--- srv/isc-agent/plugins/tcp/http/models.py
+++ srv/isc-agent/plugins/tcp/http/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData, ForeignKey
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, Text
 from sqlalchemy.orm import declarative_base
 
 
@@ -12,10 +12,42 @@ def build_Models():
     metadata_obj = MetaData()
 
     Base = declarative_base()
-
+    # Sigs model
     class Sigs(Base):
         __tablename__ = 'Sigs'
 
         id = Column(Integer, primary_key=True)
+        #Should this be Text vs String?
+        patternDescription = Column(Text)
+        patternString = Column(Text)
+        db_ref = Column(Text)
+        module = Column(Text)
+
+        def __repr__(self):
+            return f'''Sigs(id={self.id!r}, patternDescription={self.patternDescription!r}, 
+            patternString={self.patternString!r}, db_ref={self.db_ref!r}, module={self.module!r} )'''
+
+    class HdrResponses(Base):
+        __tablename__ = 'HdrResponses'
+
+        id = Column(Integer)
+        SigID = Column(Integer)
+        HeaderField = Column(Text)
+        dataField = Column(Text)
+
+        def __repr__(self):
+            return f'''HdrResponses(id={self.id!r}, SigID={self.SigID!r}, 
+            HeaderField={self.HeaderField}, dataField={self.dataField!r}'''
+
+    class paths(Base):
+        __tablename__ = 'paths'
+
+        SigID = Column(Integer)
+        SQLInput = Column(Text)
+        SQLOutput = Column(Text)
+
+        def __repr__(self):
+            return f"paths(SigID={self.SigID!r}, SQLInput={self.SQLInput!r}, SQLOutput={self.SQLOutput!r}"
+
 
 
