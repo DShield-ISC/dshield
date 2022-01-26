069dfd3a24bd44093d702da886a2ac92b8477ef9
jdrew-sans <jdrew@sans.org>
2022-01-26 12:43:04 -0600

    ISC-10 #WIP Completed models in models.py.

--- srv/isc-agent/plugins/tcp/http/models.py
+++ srv/isc-agent/plugins/tcp/http/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData, ForeignKey
-from sqlalchemy import Table, Column, Integer, String, Text
+from sqlalchemy import Table, Column, Integer, String, Text, BLOB
 from sqlalchemy.orm import declarative_base
 
 
@@ -12,12 +12,13 @@ def build_Models():
     metadata_obj = MetaData()
 
     Base = declarative_base()
+
     # Sigs model
     class Sigs(Base):
         __tablename__ = 'Sigs'
 
         id = Column(Integer, primary_key=True)
-        #Should this be Text vs String?
+        # Should this be Text vs String?
         patternDescription = Column(Text)
         patternString = Column(Text)
         db_ref = Column(Text)
@@ -37,17 +38,139 @@ def build_Models():
 
         def __repr__(self):
             return f'''HdrResponses(id={self.id!r}, SigID={self.SigID!r}, 
-            HeaderField={self.HeaderField}, dataField={self.dataField!r}'''
+            HeaderField={self.HeaderField}, dataField={self.dataField!r})'''
 
     class paths(Base):
         __tablename__ = 'paths'
+
+        SigID = Column(Integer)
+        path = Column(Text)
+        OSPath = Column(Text)
+
+        def __repr__(self):
+            return f"paths(SigID={self.SigID!r}, path={self.path!r}, OSPath={self.OSPath!r})"
+
+    class SQLResp(Base):
+        __tablename = 'SQLResp'
 
         SigID = Column(Integer)
         SQLInput = Column(Text)
         SQLOutput = Column(Text)
 
         def __repr__(self):
-            return f"paths(SigID={self.SigID!r}, SQLInput={self.SQLInput!r}, SQLOutput={self.SQLOutput!r}"
+            return f"SQLResp(SigID={self.SigID!r}, SQLInput={self.SQLInput!r}, SQLOutput={self.SQLOutput!r})"
+
+    class XssResp(Base):
+        __tablename__ = 'XSSResp'
+
+        SigID = Column(Integer)
+        ScriptReq = Column(Text, primary_key=True)
+        ScriptResp = Column(Text)
+
+        def __repr__(self):
+            return f"XSSResp(SigID={self.SigID!r}, ScriptReq={self.ScriptReq!r}, ScriptResp={self.ScriptResp!r})"
+
+    class RFIResp(Base):
+        __tablename__ = 'RFIResp'
+
+        SigID = Column(Integer)
+        protocol = Column(Text, primary_key=True)
+        remoteuri = Column(Text)
+
+        def __repr__(self):
+            return f"RFIResp(SigID={self.SigID!r}, protocol={self.protocol!r}, remoteuri={self.remoteuri!r})"
+
+    class FileResp(Base):
+        __tablename__ = 'FileResp'
+
+        ID = Column(Integer)
+        SigID = Column(Integer)
+        FileNamePost = Column(Text)
+        FileDataPost = Column(BLOB)
+        FileTextPost = Column(Text)
+        OSPath = Column(Text)
+        FileResp = Column(BLOB)
+        CowrieRef = Column(Text)
+
+        def __repr__(self):
+            return f'''FileResp(ID={self.ID!r}, SigID={self.SigID!r}, FileNamePost={self.FileNamePost!r},
+            FileDataPost={self.FileDataPost!r}, FileTextPost={self.FileTextPost!r}, OSPath={self.OSPath!r},
+            FileResp={self.FileResp!r}, CowrieRef={self.CowrieRef!r})'''
+
+    class Postlogs(Base):
+        __tablename__ = 'postlogs'
+
+        ID = Column(Integer, primary_key=True)
+        data = Column(Text)
+        headers = Column(Text)
+        address = Column(Text)
+        cmd = Column(Text)
+        path = Column(Text)
+        useragent = Column(Text)
+        vers = Column(Text)
+        formkey = Column(Text)
+        formvalue = Column(Text)
+        summary = Column(Text)
+
+        def __repr__(self):
+            return f'''postlogs(ID={self.ID!r}, data={self.data!r}, headers={self.headers!r}, address={self.address!r}
+            cmd={self.cmd!r}, path={self.path!r}, useragent={self.useragent!r}, vers={self.vers!r}, 
+            formkey={self.formkey!r}, fomrvalue={self.formvalue!r}, summary={self.summary!r})'''
+
+    class Files(Base):
+        __tablename__ = 'files'
+
+        ID = Column(Integer, primary_key=True)
+        RID = Column(Integer)
+        filename = Column(Text)
+        DATA = Column(BLOB)
+
+        def __repr__(self):
+            return f"files(ID={self.ID!r}, RID={self.RID!r}, filename={self.filename!r}, DATA={self.DATA!r})"
+
+    class Requests(Base):
+        __tablename__ = 'requests'
+
+        data = Column(Text)
+        headers = Column(Text)
+        address = Column(Text)
+        cmd = Column(Text)
+        path = Column(Text)
+        useragent = Column(Text)
+        vers = Column(Text)
+        summery = Column(Text)
+        target = Column(Text)
+
+        def __repr__(self):
+            return f'''requests(data={self.data!r}, headers={self.headers!r}, address={self.address!r}, 
+            cmd={self.cmd!r}, path={self.path!r}, useragent={self.useragent!r}, vers={self.vers!r},
+            summary={self.summery!r}, target={self.target!r})'''
+
+    class Useragents(Base):
+        __tablename__ = 'useragents'
+
+        ID = Column(Integer, primary_key=True)
+        refid = Column(Integer)
+        useragent = Column(Text, unique=True)
+
+        def __repr__(self):
+            return f"useragents(ID={self.ID!r}, refid={self.refid!r}, useragent={self.useragent!r})"
+
+    class Responses(Base):
+        __tablename__ = 'responses'
+
+        ID = Column(Integer, primary_key=True)
+        RID = Column(Integer)
+        HeaderField = Column(Text)
+        dataField = Column(Text)
+
+        def __repr__(self):
+            return f'''reponses(ID={self.ID!r}, RID={self.RID!r}, 
+            HeaderField={self.HeaderField!r}, dataField={self.dataField!r})'''
+
+        
+
+
 
 
 
