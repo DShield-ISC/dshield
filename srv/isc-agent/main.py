b205b36e6fa3d84870bc68316abd936f7ac5904b
jdrew-sans <jdrew@sans.org>
2022-02-27 19:06:52 -0600

    ISC-26 #ISC-Agent submit to task with timing loop

--- srv/isc-agent/main.py
+++ srv/isc-agent/main.py
@@ -1,7 +1,8 @@
 import importlib
 import logging
 
-from twisted.internet import reactor
+from twisted.internet import task, reactor
+from plugins.tcp.http import iscagentsubmit
 
 import settings
 
@@ -14,5 +15,8 @@ if __name__ == '__main__':
         logger.debug('%s options: %s', plugin['name'], plugin)
         module = importlib.import_module(f'plugins.{plugin["protocol"]}.{plugin["name"]}')
         module.handler(**plugin)
+
+    l = task.LoopingCall(iscagentsubmit.test_post)
+    l.start(10.0)
 
     reactor.run()
