4307dc2df350ab3f18d9711ce676ea5894b6eb5a
jdrew-sans <jdrew@sans.org>
2022-03-03 22:13:36 -0600

    ISC-26 #Post data

--- srv/isc-agent/plugins/tcp/http/iscagent_submit.py
+++ srv/isc-agent/plugins/tcp/http/iscagent_submit.py
@@ -1,13 +1,15 @@
 import logging
+
 import settings
 
-from twisted.internet import defer, reactor, protocol
+from twisted.internet import reactor
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
+
+
 from plugins.tcp.http import models
 
 logger = logging.getLogger(__name__)
-# body = FileBodyProducer(logs)
 send_to_dshield = True
 
 logs = []
@@ -16,12 +18,11 @@ logs = []
 def http_request(data, url=settings.DSHIELD_URL):
     logger.warning('HTTP Request')
     agent = Agent(reactor)
-
     d = agent.request(
         b"POST",
         url,
         Headers({'content-type': 'application/json', 'User-Agent': 'DShield PyLib 0.1'}),
-        None,
+        data,
     )
 
     def handle_response(response):
