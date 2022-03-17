cdbf662de8581cd0f3d1e5af230001612ea0e878
bensonmyrtil <bmyrtil@sans.org>
2022-03-17 14:14:02 -0400

    ISC-23 #Fix linting

--- srv/isc-agent/main.py
+++ srv/isc-agent/main.py
@@ -9,13 +9,12 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     logger.debug('ISC Agent starting')
-    try:
-        for plugin in settings.PLUGINS:
+    for plugin in settings.PLUGINS:
+        try:
             module = importlib.import_module(f'plugins.{plugin["protocol"]}.{plugin["name"]}')
             module.handler(**plugin)
             logger.info('Plugin %s activated', plugin['name'])
             logger.debug('%s options: %s', plugin['name'], plugin)
-    except ModuleNotFoundError:
-        logger.info('Plugin %s:%s not found', plugin['protocol'], plugin['name'])
-        pass
+        except ModuleNotFoundError:
+            logger.info('Plugin %s:%s not found', plugin['protocol'], plugin['name'])
     reactor.run()
