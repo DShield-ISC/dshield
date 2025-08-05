ecb281b3fa5eaa1de6c05e6e372b3b1322846d14
Johannes B. Ullrich <jullrich@sans.edu>
2025-08-05 11:00:17 -0400

    fixes to cleanup script

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,7 +5,12 @@
 # Cleanup script to safe disk space
 #
 #  the script will ask for confirmation, unless you
-# add the optional 'cron' commandline parameter
+# add the optional 'cron' commandline parameter.
+# If run with 'cron', only files older than 14 days are
+# deleted.
+#
+#  in addition, logs from journald older than 7 days are
+# deleted.
 #
 ####
 
@@ -17,28 +22,36 @@ if [ ! "$uid" = "0" ]; then
 fi
 
 if [[ $1 != cron ]]; then
-echo
-echo "  This script will delete backups and log files."
-echo "  Do you want to proceed? (Y/N)"
-echo
-while : ; do
-    read -s -p "Press Y/N key: " -n 1 k <&1
-    k=${k^}
-    echo $k
-    if [[ $k = N ]]; then
-	echo
-	echo "abort"	
+  echo
+  echo "  This script will delete backups and log files."
+  echo "  Do you want to proceed? (Y/N)"
+  echo
+  while : ; do
+      read -s -p "Press Y/N key: " -n 1 k <&1
+      k=${k^}
+      echo $k
+      if [[ $k = N ]]; then
+  	echo
+	echo "abort"
 	exit
-    fi
-    if [[ $k = Y ]]; then
+      fi
+      if [[ $k = Y ]]; then
 	echo
 	echo "removing logs and backups"
 	break
-    fi    
-done
+      fi
+  done
+  rm -rf /srv/www.2*
+  rm -rf /srv/dshield.2*
+  rm -rf /srv/cowrie.2*
+  rm -rf /srv/log/*
+  rm -rf /srv/cowrie/var/log/cowrie/*
+  /usr/bin/journalctl --vacuum-time=7d  
+else
+  find /srv -name 'www.2*' -ctime +14  -delete
+  find /srv -name 'dshield.2*' -ctime +14  -delete
+  find /srv -name 'cowrie.2*' -ctime +14  -delete
+  find /srv/log -ctime +14  -delete
+  find /srv/cowrie/var/log/cowrie/*  -ctime +14  -delete
+  /usr/bin/journalctl --vacuum-time=7d  
 fi
-rm -rf /srv/www.2*
-rm -rf /srv/dshield.2*
-rm -rf /srv/cowrie.2*
-rm -rf /srv/log/*
-rm -rf /srv/cowrie/var/log/cowrie/*
