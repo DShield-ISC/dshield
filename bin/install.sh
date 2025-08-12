4e92a564f2312ebe92582b2450acc81fdf30e7b3
Johannes B. Ullrich <jullrich@sans.edu>
2025-08-11 23:05:01 -0400

    fixing path for chmod

--- bin/install.sh
+++ bin/install.sh
@@ -2228,7 +2228,7 @@ sudorun "chmod 0770 $COWRIEDIR"
 cd ${COWRIEDIR} || exit
 dlog "setting up virtual environment"
 run 'sudo chgrp -R cowrie /srv/cowrie'
-run 'sudo chmod g+w cowrie-env/lib'
+run 'sudo chmod g+w /srv/cowrie/cowrie-env/lib'
 run 'sudo -u cowrie virtualenv --python=python3 cowrie-env'
 dlog "activating virtual environment"
 run 'source cowrie-env/bin/activate'
