cec8e49ee8ae79403537c1536cd1ead0b2deda3f
Johannes B. Ullrich <jullrich@sans.edu>
2025-11-04 10:47:44 -0500

    fixing path of ini file and adding piid in case it is missing

--- bin/updatehoneypotip.sh
+++ bin/updatehoneypotip.sh
@@ -8,13 +8,17 @@ if [ ! "$userid" = "0" ]; then
   exit 9
 fi
 
-if [ ! -f /etc/dshield.ini ]; then
-    echo "missing /etc/dshield.ini file"
+if [ ! -f /srv/dshield/etc/dshield.ini ]; then
+    echo "missing /srv/dshield/etc/dshield.ini file"
     exit 9
 fi
 honeypotip=$(curl -s https://www4.dshield.org/api/myip?json | jq .ip | tr -d '"')
 if echo -n $honeypotip | egrep -q '^[0-9\.]+$'; then
-    sed -i "s/^honeypotip=.*/honeypotip=$honeypotip/" /etc/dshield.ini
+    sed -i "s/^honeypotip=.*/honeypotip=$honeypotip/" /srv/dshield/etc/dshield.ini
+    if ! grep -q '^piid=' dshield.ini; then
+	piid=$(openssl rand -hex 10)
+	sed -i "^apikey/a piid=$piid"  /srv/dshield/etc/dshield.ini
+    fi
 else
     echo "Bad IP address"
     exit 9
