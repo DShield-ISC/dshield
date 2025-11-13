cd8073d1f41d2f698091eec20e9082d16a4b25a7
root <root@rpi4tumhon.fritz.box>
2025-11-13 15:35:12 +0100

    modified:   bin/updatehoneypotip.sh  - fixed egrep to grep -E and /etc/dshield.ini new files:  - etc/cowrie.pp  - etc/cowrie1.pp  - srv/cowrie/bin/cowrie

--- bin/updatehoneypotip.sh
+++ bin/updatehoneypotip.sh
@@ -15,6 +15,11 @@ fi
 honeypotip=$(curl -s https://www4.dshield.org/api/myip?json | jq .ip | tr -d '"')
 if echo -n $honeypotip | grep -E -q '^[0-9\.]+$'; then
     sed -i "s/^honeypotip=.*/honeypotip=$honeypotip/" /etc/dshield.ini
+    if ! grep -q '^piid=' /etc/dshield.ini; then
+       piid=$(openssl rand -hex 10)
+       sed -i "^apikey/a piid=$piid"  /etc/dshield.ini
+    fi
+    cp /etc/dshield.ini /srv/dshield/etc/
 else
     echo "Bad IP address"
     exit 9
